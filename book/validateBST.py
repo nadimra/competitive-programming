0a3350e0e5ba64416238aeb70e37fd5694377686
nadimra <nadimrahman98@gmail.com>
2021-12-26 19:21:42 +0000

    validate bst

--- book/validateBST.py
+++ book/validateBST.py
@@ -1,21 +1,25 @@
 import binaryTree
 import bst
 
-"""
+
 g = binaryTree.Graph()
+
+"""
 nodeH = binaryTree.Node(8)
 nodeG = binaryTree.Node(7)
 nodeF = binaryTree.Node(6)
 nodeE = binaryTree.Node(5)
-nodeD = binaryTree.Node(4)
-nodeC = binaryTree.Node(3)
-nodeB = binaryTree.Node(2)
-nodeA = binaryTree.Node(1)
+"""
+nodeD = binaryTree.Node(10)
+nodeC = binaryTree.Node(12)
+nodeB = binaryTree.Node(4)
+nodeA = binaryTree.Node(8)
 
 nodeA.addChildLeft(nodeB)
 nodeA.addChildRight(nodeC)
-nodeB.addChildLeft(nodeD)
-nodeD.addChildRight(nodeE)
+nodeC.addChildLeft(nodeD)
+#nodeB.addChildLeft(nodeD)
+#nodeD.addChildRight(nodeE)
 #nodeE.addChildLeft(nodeF)
 #nodeF.addChildLeft(nodeG)
 #nodeF.addChildRight(nodeH)
@@ -23,6 +27,7 @@ nodeD.addChildRight(nodeE)
 g.addNode(nodeA)
 g.addNode(nodeB)
 g.addNode(nodeC)
+"""
 g.addNode(nodeD)
 g.addNode(nodeE)
 g.addNode(nodeF)
@@ -30,23 +35,33 @@ g.addNode(nodeG)
 g.addNode(nodeH)
 """
 
-def validateBST(root):
+def validateBST(root,allowableMin,allowableMax):
     if root.children == [None,None]:
         return True
     else:
         leftChild = root.children[0]
         rightChild = root.children[1]
-        if leftChild is not None:
-            if leftChild.value <= root.value:
-                return True and validateBST(leftChild)
+        if leftChild is not None and rightChild is not None:
+            leftCondition = leftChild.value <= root.value and leftChild.value >=allowableMin
+            rightCondition = rightChild.value > root.value and rightChild.value < allowableMax
+            if leftCondition and rightCondition:
+                return True and validateBST(leftChild,allowableMin,root.value) and validateBST(rightChild,root.value,allowableMax)
             else:
                 return False
-        if rightChild is not None:
-            if rightChild.value > root.value:
-                return True and validateBST(rightChild)
+        elif leftChild is not None:
+            leftCondition = leftChild.value <= root.value and leftChild.value >=allowableMin
+            if leftCondition:
+                return True and validateBST(leftChild,allowableMin,root.value)
+            else:
+                return False
+        elif rightChild is not None:
+            rightCondition = rightChild.value > root.value and rightChild.value < allowableMax
+            if rightCondition:
+                return True and validateBST(rightChild,root.value,allowableMax)
             else:
                 return False
 
-numArray = [3,4,2,7,2,10,5]
-g,root = bst.createBinarySearchTree(numArray)
-print(validateBST(root))
+
+#numArray = [3,4,2,7,2,10,5]
+#g,root = bst.createBinarySearchTree(numArray)
+print(validateBST(nodeA,float('-inf'),float('inf')))
